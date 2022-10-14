b7602df97ea1bb7c937804af5d577c174231c899
Jennifer Gifford <jgifford82@gmail.com>
2022-10-13 20:36:26 -0600

    commit tree 66493388454175c32835267f2cb474aa8805779a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :num1, :num2, :num3
+
+  def initialize(num1, num2, num3)
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
+  end
+
+  def kind
+    if num1 == 0 && num2 == 0 && num3 == 0
+      # binding.pry
+      raise TriangleError
+      # binding.pry
+    elsif num1.negative? || num2.negative? || num3.negative?
+      raise TriangleError
+    elsif !triangle
+      raise TriangleError
+    # elsif num1 + num2 < num3 && num1 + num3 < num2 && num2 + num3 < num1
+      # binding.pry
+      # raise TriangleError
+      # binding.pry
+    elsif num1 == num2 && num2 == num3
+      :equilateral
+      # binding.pry
+    elsif num1 == num2 || num2 == num3 || num1 == num3
+      :isosceles
+    elsif num1 != num2 && num2 != num3
+      :scalene
+      # binding.pry
+    end
+  end
+
+  def triangle
+    num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1
+  end
+
+  class TriangleError < StandardError
+  end
 end
